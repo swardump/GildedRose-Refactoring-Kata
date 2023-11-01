a79a3023412289369d71d552d9bb65058123775b
Semen Loshchakov <loschakov.semen@gmail.com>
2023-11-01 20:41:35 +0400

    complete cata and unit tests

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -17,51 +17,194 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            $item = new EntityFactory($item);
+        }
+    }
+}
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
+/**
+ * Класс управляющий созданием объектов в зависимости от наименования товара.
+ */
+final class EntityFactory
+{
+    public function __construct(
+        public Item $item
+    ) {
+        $item = match (true) {
+            str_starts_with($item->name, 'Sulfuras') => $this->getSulfuras($item),
+            str_starts_with($item->name, 'Conjured') => $this->getConjured($item),
+            str_starts_with($item->name, 'Aged Brie') => $this->getAgedBrie($item),
+            str_starts_with($item->name, 'Backstage passes') => $this->getBackstagePasses($item),
+            default => $this->getBaseItem($item)
+        };
+    }
 
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+    private function getSulfuras(Item $item): object
+    {
+        return new Sulfuras($item);
+    }
+
+    private function getConjured(Item $item): object
+    {
+        return new Conjured($item);
+    }
+
+    private function getAgedBrie(Item $item): object
+    {
+        return new AgedBrie($item);
+    }
+
+    private function getBackstagePasses(Item $item): object
+    {
+        return new BackstagePasses($item);
+    }
+
+    private function getBaseItem(Item $item): object
+    {
+        return new BaseItem($item);
+    }
+}
+
+/**
+ * Базовый класс для обычных товаров, определяющий стандартные свойства и методы.
+ */
+class BaseItem
+{
+    public int $index = 1;
+
+    public function __construct(
+        public Item $item
+    ) {
+        $this->decreaseQuality($item);
+        $this->decreaseSellIn($item);
+    }
+
+    protected function increaseQuality(Item $item): void
+    {
+        if ($item->quality < 50) {
+            $item->quality = $item->quality + $this->index;
+        }
+    }
+
+    protected function decreaseQuality(Item $item): void
+    {
+        if ($item->quality > 0) {
+            $item->quality = $item->quality - $this->index;
+        }
+    }
+
+    protected function decreaseSellIn(Item $item): void
+    {
+        if ($item->sellIn > 0) {
+            $item->sellIn = $item->sellIn - $this->index;
+        } else {
+            $item->sellIn = $item->sellIn - $this->index;
+            $this->decreaseQuality($item);
+        }
+    }
+}
+
+/**
+ * Данный класс не требует наследования от базового класса итемов по условиям задачи.
+ * т.к. качество качество всегда является постоянным значением = 80, независимо от переданного значения в конструктор,
+ * срок хранения остается равным переданному значению в конструкторю
+ * «Sulfuras» является легендарным товаром, поэтому у него нет срока хранения и не подвержен ухудшению качества;
+ * легендарный товар «Sulfuras» имеет качество 80 и оно никогда не меняется.
+ */
+class Sulfuras extends BaseItem
+{
+    private const QUALITY = 80;
+
+    public function __construct(
+        public Item $item
+    ) {
+        $item->quality = self::QUALITY;
+    }
+}
+
+/**
+ * Наследуем свойства и методы обычных товаров, увеличивая индекс качества в два раза, в соответствии с условиями задачи.
+ * «Conjured» товары теряют качество в два раза быстрее, чем обычные товары.
+ */
+class Conjured extends BaseItem
+{
+    public function __construct(
+        public Item $item
+    ) {
+        $this->decreaseSellIn($item);
+        $this->decreaseQuality($item);
+        $this->decreaseQuality($item);
+    }
+}
+
+
+/**
+ * Наследуем свойства и методы обычных товаров. Переопределяем функцию increaseQuality.
+ * т.к. необходимы дополнительные условия для изменения качества.
+ * Определение свойств качества аналогично с классом BackstagePasses.
+ * Для товара «Aged Brie» качество увеличивается пропорционально возрасту;
+ */
+class AgedBrie extends BaseItem
+{
+    public function __construct(
+        public Item $item
+    ) {
+        $this->decreaseSellIn($item);
+        $this->increaseQuality($item);
+    }
+
+    protected function increaseQuality(Item $item): void
+    {
+        if ($item->sellIn >= 0 && $item->quality < 50) {
+            $item->quality = $item->quality + $this->index;
+        }
+        if ($item->sellIn < 0 && $item->quality < 50) {
+            $item->quality = $item->quality + $this->index * 2 > 50 ? 50 : $item->quality + $this->index * 2;
+        }
+    }
+
+    protected function decreaseSellIn(Item $item): void
+    {
+        $item->sellIn = $item->sellIn - 1;
+    }
+}
+
+
+/**
+ * Наследуем свойства и методы обычных товаров, переопределяем функции increaseQuality и decreaseSellIn,
+ * т.к. необходимы дополнительные условия для изменения качества.
+ * Качество «Backstage passes» также, как и «Aged Brie», увеличивается по мере приближения к сроку хранения.
+ * Качество увеличивается на 2, когда до истечения срока хранения 10 или менее дней и на 3,
+ * если до истечения 5 или менее дней. При этом качество падает до 0 после даты проведения концерта.
+ */
+class BackstagePasses extends BaseItem
+{
+    public function __construct(
+        public Item $item
+    ) {
+        $this->increaseQuality($item);
+        $this->decreaseSellIn($item);
+    }
+
+    protected function increaseQuality(Item $item): void
+    {
+        if ($item->sellIn < 6) {
+            $item->quality = $item->quality + $this->index * 3 > 50 ? 50 : $item->quality + $this->index * 3;
+        } elseif ($item->sellIn < 11) {
+            $item->quality = $item->quality + $this->index * 2 > 50 ? 50 : $item->quality + $this->index * 2;
+        } else {
+            $item->quality = $item->quality + $this->index >= 50 ? 50 : $item->quality + $this->index;
+        }
+    }
+
+    protected function decreaseSellIn(Item $item): void
+    {
+        if ($item->sellIn <= 0) {
+            $item->quality = 0;
+            $item->sellIn = $item->sellIn - $this->index;
+        }
+        if ($item->sellIn > 0) {
+            $item->sellIn = $item->sellIn - $this->index;
         }
     }
 }
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -10,11 +10,62 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    public function testBaseItem(): void
     {
-        $items = [new Item('foo', 0, 0)];
+        $items = [
+            new Item('Elixir of the Mongoose', 0, 0),
+            new Item('Elixir of the Mongoose', -1, 20),
+        ];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame(0, $items[0]->quality); //Качество не может быть ниже 0
+        $this->assertSame(18, $items[1]->quality); //При снижении срока хранения ниже 0, качество уменьшается в 2 раза быстрее
+    }
+
+    public function testSulfurasItem(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', -1, 50)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(-1, $items[0]->sellIn); //Срок хранения всегда остается неизменным
+        $this->assertSame(80, $items[0]->quality); //Качество всегда равно 80
+    }
+
+    public function testConjuredItem(): void
+    {
+        $items = [new Item('Conjured Mana Cake', 1, 4)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(2, $items[0]->quality); //Качество уменьшается в 2 раза быстрее чем у обычных товаров
+    }
+
+    public function testAgedBrieItem(): void
+    {
+        $items = [
+            new Item('Aged Brie', 2, 0),
+            new Item('Aged Brie', -1, 0),
+            new Item('Aged Brie', -1, 49),
+        ];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(1, $items[0]->quality); //Качество увеличивается пропорционально возрасту
+        $this->assertSame(2, $items[1]->quality); //Качество увеличивается пропорционально возрасту
+        $this->assertSame(50, $items[2]->quality); //Качество не может быть больше 50
+    }
+
+    public function testBackstagePassesItem(): void
+    {
+        $items = [
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 0),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 0),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 0),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 50),
+        ];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(1, $items[0]->quality); //Качество увеличивается по мере приближения к сроку хранения на 1
+        $this->assertSame(2, $items[1]->quality); //Качество увеличивается на 2, когда до истечения срока хранения 10 или менее дней
+        $this->assertSame(3, $items[2]->quality); //Качество увеличивается на 3, если до истечения 5 или менее дней
+        $this->assertSame(0, $items[3]->quality); //Качество падает до 0 после даты проведения концерта
     }
 }
